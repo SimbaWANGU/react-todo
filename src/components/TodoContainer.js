9ad20abca19e6a5533e2ac6a316e2e33cba77382
SimbaWANGU <simbawangulu@gmail.com>
2022-05-30 12:49:13 +0300

    Create component files in project

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TodoContainer extends Component {
+  render() {
+    return (
+      <div>TodoContainer</div>
+    )
+  }
+}
+
+export default TodoContainer
