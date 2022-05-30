1bf2cfeb91a6f12e005961c6f5f65992d1777610
Simba Wangu <simbawangulu@gmail.com>
2022-05-30 16:28:59 +0300

    Revert "Add functionality"

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,81 +1,10 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable react/no-access-state-in-setstate */
-/* eslint-disable class-methods-use-this */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import Header from './Header';
-import InputTodo from './InputTodo';
-import TodoList from './TodoList';
 
 class TodoContainer extends Component {
-  state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and content',
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: true,
-      },
-    ],
-  };
-
-  handleChange = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          };
-        }
-        return todo;
-      }),
-    }));
-  };
-
-  delToDo = (id) => {
-    this.setState({
-      todos: [
-        ...this.state.todos.filter((todo) => {
-          return todo.id !== id;
-        }),
-      ],
-    });
-  };
-
-  addToDoItem = (title) => {
-    const newToDo = {
-      id: uuidv4(),
-      title,
-      completed: false,
-    };
-    this.setState({
-      todos: [...this.state.todos, newToDo],
-    });
-  };
-
   render() {
     return (
-      <>
-        <Header />
-        <InputTodo addToDoProps={this.addToDoItem} />
-        <TodoList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteToDoProps={this.delToDo}
-        />
-      </>
+      <div>TodoContainer</div>
     );
   }
 }
